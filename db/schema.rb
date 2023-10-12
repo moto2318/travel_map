63ab6895ad92b73e8bc31b1362f826f4341035af
EC2 Default User <ec2-user@ip-172-31-31-237.ap-northeast-1.compute.internal>
2023-10-12 12:33:35 +0000

    ポップアップの投稿機能完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_09_131744) do
+ActiveRecord::Schema.define(version: 2023_10_12_105430) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2023_10_09_131744) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "pins", force: :cascade do |t|
