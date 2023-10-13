b56053c51264188a36d07c4342e4465efdde2ec8
EC2 Default User <ec2-user@ip-172-31-31-237.ap-northeast-1.compute.internal>
2023-10-13 12:25:38 +0000

    会員機能ー途中経過１

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_12_105430) do
+ActiveRecord::Schema.define(version: 2023_10_13_122208) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_10_12_105430) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
