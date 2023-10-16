51b43b27cf312b89d08fe24be5d19d7f4569dfdc
moto2318 <suigekka0101@gmail.com>
2023-10-16 06:48:15 +0000

    テストデプロイの環境構築（gem導入）

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,20 +1,28 @@
 class Public::CustomersController < ApplicationController
 
   def index
+    @coustomer = current_customer
     @lists = Map.all
   end
 
 
   def show
-    @list = Map.find(params[:id])
+    @coustomer = current_customer
+    #@list = Map.find(params[:id])
   end
 
   def edit
-    @list = Map.find(params[:id])
+    @coustomer = current_customer
+    #@list = Map.find(params[:id])
   end
 
   def update
-
+     @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :edit
+    end
   end
 
   def confirm
@@ -22,10 +30,17 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
-
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
   private
+
+    def customer_params
+      params.require(:customer).permit(:name, :email, :is_deleted)
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_map
       @map = Map.find(params[:id])
